db42b2e69a7082ef53a98265f2543a23b9dce8db
Mech <mechsix@users.noreply.github.com>
2022-05-23 23:16:16 +0800

    docs: Update page edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,16 +36,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/NoobTechNote/NoobTechNote.github.io/tree/main',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
